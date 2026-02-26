9f59a17e54b3e15111236f7981ba848d71fa9345
antoniojvargas <toyoyo600@gmail.com.com>
2026-02-26 18:08:04 -0400

    Add Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,20 @@ async function bootstrap() {
       transform: true, // Transforma el payload al tipo del DTO
     }),
   );
+
+  // --- CONFIGURACIÓN SWAGGER ---
+  const config = new DocumentBuilder()
+    .setTitle('Smart Parking API')
+    .setDescription(
+      'Sistema de gestión de entradas, salidas y costos de parqueo',
+    )
+    .setVersion('1.0')
+    .addTag('vehicles')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // La URL será http://localhost:3000/api
+  // -----------------------------
 
   await app.listen(process.env.PORT ?? 3000);
 }
--- src/vehicles/dto/create-vehicle.dto.ts
+++ src/vehicles/dto/create-vehicle.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEnum, IsNotEmpty, IsString, Matches } from 'class-validator';
 
 export enum VehicleType {
@@ -6,11 +7,17 @@ export enum VehicleType {
 }
 
 export class CreateVehicleDto {
+  @ApiProperty({ example: 'ABC-123', description: 'Placa del vehículo' })
   @IsString()
   @IsNotEmpty()
   @Matches(/^[A-Z0-9-]{6,10}$/, { message: 'Placa inválida (ej: ABC-123)' })
   plate: string;
 
+  @ApiProperty({
+    example: 'car',
+    enum: ['car', 'motorcycle'],
+    description: 'Tipo de vehículo',
+  })
   @IsEnum(VehicleType, { message: 'El tipo debe ser car o motorcycle' })
   type: VehicleType;
 }
